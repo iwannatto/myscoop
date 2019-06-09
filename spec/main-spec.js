6390861158ab7156e136eebf955f855c4f237583
iwannatto <iwannatto@gmail.com>
2019-06-09 15:13:18 +0900

    Click interface

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -34,29 +34,41 @@ describe('Main', () => {
 
     describe('select specific line, dispatch myscoop:scoop', () => {
       let selectedLine = 16;
+      let scoopEditor;
       beforeEach(() => {
         sourceEditor.setCursorBufferPosition([selectedLine, 0]);
         atom.commands.dispatch(workspaceElement, 'myscoop:scoop');
         waitsForPromise(() => { return activationPromise; });
-        waitsFor(() => atom.workspace.getActivePaneItem().getPath() === scoopFileName);
+        waitsFor(() => {
+          if (atom.workspace.getActivePaneItem().getPath() === scoopFileName) {
+            scoopEditor = atom.workspace.getActiveTextEditor();
+            return true;
+          } else {
+            return false;
+          }
+        });
       });
 
       it('copies above main', () => {
-        let scoopEditor = atom.workspace.getActiveTextEditor();
         for (let i = 0; i < 2; ++i) {
           expect(scoopEditor.lineTextForBufferRow(i)).toBe(sourceEditor.lineTextForBufferRow(i));
         }
       })
 
       it('copies selected line to scoop editor', () => {
-        let scoopEditor = atom.workspace.getActiveTextEditor();
         expect(scoopEditor.lineTextForBufferRow(3)).toBe(sourceEditor.lineTextForBufferRow(selectedLine));
       });
 
       it('marks undefined variable', () => {
-        let scoopEditor = atom.workspace.getActiveTextEditor();
         expect(scoopEditor.findMarkers({})).not.toEqual([]);
+        jasmine.attachToDOM(workspaceElement);
+        expect(workspaceElement.querySelector('.highlight')).not.toBeNull();
       });
+
+      it('shows suggestion', () => {
+        jasmine.attachToDOM(workspaceElement);
+        expect(workspaceElement.querySelector('.suggestion')).not.toBeNull();
+      })
     });
   });
 });
